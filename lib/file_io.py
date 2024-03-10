b1c183ccf1dcc10bf58da8043cd805efda1cc2f3
Klif <sang.wicklife@student.moringaschool.com>
2024-03-10 21:21:13 +0300

    file manipulation

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_path, content):
+    """Write content to a file."""
+    with open(f'{file_path}.txt', 'w') as f:
+        f.write(content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_path, content):
+    """Append content to a file."""
+    with open(f'{file_path}.txt', 'a') as f:
+        f.write(content)
 
-def read_file(file_name):
-    pass
+def read_file(file_path):
+    """Read content from a file."""
+    with open(f'{file_path}.txt', 'r') as f:
+        return f.read()
